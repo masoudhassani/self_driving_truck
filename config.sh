664d20c4d394494641d9508c120484fc9772c144
masoudhassani <masoudhassani@users.noreply.github.com>
2020-06-28 11:19:41 -0400

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,7 @@
+# installing required packages system wide
+sudo apt -y install libjasper1
+sudo apt -y install libatlas-base-dev 
+sudo apt -y install libqtgui4 libqt4-test libqtcore4
+
+# install required python packages
+python3 -m pip install -r requirments.txt
