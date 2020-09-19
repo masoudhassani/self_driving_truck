d3d230db8ca75e4d86e39a860ba94b906d282ff7
masoudhassani <m.hasany@gmail.com>
2020-09-19 11:12:58 -0400

    added thresholding

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-from modules.utils import calibrate_camera
+from modules.utils import *
 
 # get all camera calibration images and calibrate the camera
 ret, cMat, coefs, rvects, tvects = calibrate_camera('resources/')
-print(ret, cMat, coefs, rvects, tvects)
-print(type(coefs))
+
+# undistort the frame
+frame = mpimg.imread('./resources/test_images/test1.jpg')
+undistorted_frame = undistort(frame, cMat, coefs)
+
+# create the threshold frame
+threshold_img = get_threshold(undistorted_frame, show=False)
+cv2.imwrite('./threshold_test2.jpg', threshold_img)
+
+warped, M, Minv = warp_to_lines(threshold_img, show=False)
+cv2.imwrite('./warped.jpg', warped)
