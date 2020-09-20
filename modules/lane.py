3373368555927f1eadf87100fb023e075def3391
masoudhassani <m.hasany@gmail.com>
2020-09-20 15:39:57 -0400

    implemented lane detection class

---
+++ modules/lane.py
@@ -0,0 +1,9 @@
+class Lane():
+    def __init__(self):
+        self.y_vals = None
+        self.left_x_vals = None
+        self.right_x_vals = None
+        self.lane_lines_img = None
+        self.left_curvature = None
+        self.right_curvature = None
+        self.offset = None
